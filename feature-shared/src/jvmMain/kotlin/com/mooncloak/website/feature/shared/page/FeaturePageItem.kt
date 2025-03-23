231cdfa4bd21b389d7d8e498318c2137ec0a8414
Chris Keenan <10093880+chRyNaN@users.noreply.github.com>
2025-03-23 13:21:39 -0400

    Created Feature.toPageItem function to convert to a FeaturePageItem

--- feature-shared/src/jvmMain/kotlin/com/mooncloak/website/feature/shared/page/FeaturePageItem.kt
+++ feature-shared/src/jvmMain/kotlin/com/mooncloak/website/feature/shared/page/FeaturePageItem.kt
@@ -7,8 +7,27 @@ public data class FeaturePageItem public constructor(
     public val path: String,
     public val title: String = feature.title,
     public val description: String? = feature.description,
-    public val keywords: String? = null,
+    public val keywords: List<String> = feature.keywords,
     public val cardImage: String? = null,
     public val headerVisible: Boolean = false,
     public val footerVisible: Boolean = false
+)
+
+public fun Feature.toPageItem(
+    path: String = this.pathPart,
+    title: String = this.title,
+    description: String? = this.description,
+    keywords: List<String> = this.keywords,
+    cardImage: String? = null,
+    headerVisible: Boolean = false,
+    footerVisible: Boolean = false
+): FeaturePageItem = FeaturePageItem(
+    feature = this,
+    path = path,
+    title = title,
+    description = description,
+    keywords = keywords,
+    cardImage = cardImage,
+    headerVisible = headerVisible,
+    footerVisible = footerVisible
 )
